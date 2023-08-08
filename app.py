1c67b3a7f3904f6156e1c83838cbefda37659cc5
MagicSakuraD <ckgoforit@outlook.com>
2023-08-08 16:48:05 +0800

    add a button to change flask var

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
-
+import logging
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+app.logger.setLevel(logging.DEBUG)
+
 
 
 
@@ -29,6 +31,15 @@ def handle_chat_message(msg):
 @socketio.on('disconnect')
 def test_disconnect():
     print('Client disconnected')
+
+num = "old value北极🤞"
+app.logger.debug(num)
+@socketio.on('change value')
+def handle_change_value(new_value):
+    # 在这里处理接收到的新值，例如修改相应的变量
+    num = new_value
+    app.logger.debug(num)
+
 
 
 if __name__ == '__main__':
